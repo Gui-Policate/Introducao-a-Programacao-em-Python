b071242e1b1f73e450bc1a2e7d1fd74285e69d3d
Gui-Policate <103331414+Gui-Policate@users.noreply.github.com>
2024-10-10 08:51:31 -0300

    Update contador_de_multiplos.py

--- contador_de_multiplos.py
+++ contador_de_multiplos.py
@@ -1,7 +1,7 @@
 print("\n")
-print("Olá, esse é um contador de multiplos")
-print("Seu objetivo é verificar quanto multiplos um numero tem \n")
-numero = float(input("Por favor informe o numero que deseja consultar: "))
+print("Olá, esse é um contador de múltiplos")
+print("Seu objetivo é verificar quanto múltiplos um número tem \n")
+numero = float(input("Por favor informe o número que deseja consultar: "))
 contador = 1
 multiplo = 0
 while contador <= numero:
@@ -9,4 +9,4 @@ while contador <= numero:
     contador = contador + 1
     if resto == 0: 
         multiplo = multiplo + 1
-print("O numero",numero ,"tem",multiplo ,"multiplos.")
+print("O número",numero ,"tem",multiplo ,"múltiplos.")
