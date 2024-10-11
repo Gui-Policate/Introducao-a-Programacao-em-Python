1a7798c08868e2c2297b8a2f1690d2b8c5742721
Gui-Policate <103331414+Gui-Policate@users.noreply.github.com>
2024-10-11 14:19:35 -0300

    Update contador_de_multiplos.py

--- contador_de_multiplos.py
+++ contador_de_multiplos.py
@@ -1,16 +1,23 @@
 print("\n")
 print("Olá, esse é um contador de múltiplos")
-print("Seu objetivo é verificar quanto múltiplos um número tem \n")
+print("Seu objetivo é verificar quantos múltiplos um número tem \n")
 
 resposta = "s"
 while resposta == "S" or resposta == "s":
     numero = float(input("Por favor informe o número que deseja consultar: "))
     print ("O numero irformado é", numero,".")
     resposta = str(input("Esta correto, S(s) ou N(n)?"))
+    while (resposta != "S" and resposta != "s" and resposta != "N" and resposta != "n"):
+        resposta = str(input("Esta correto, S(s) ou N(n)?: "))
     while resposta == "N" or resposta == "n":
-        numero = float(input("Por favor informe o número que deseja consultar: ""\n"))
+        numero = float(input("Por favor informe o número que deseja consultar: "))
         print ("O numero irformado é", numero,)
-        resposta = str(input("esta correto, S(s) ou N(n)?"))
+        resposta = str(input("Esta correto, S(s) ou N(n)?: "))
+        while (resposta != "S" and resposta != "s" and resposta != "N" and resposta != "n"):
+            resposta = str(input("Esta correto, S(s) ou N(n)?: "))
+
+    print("\n")
+
     contador = 1
     multiplo = 0
     while contador <= numero:
@@ -18,6 +25,11 @@ while resposta == "S" or resposta == "s":
         contador = contador + 1
         if resto == 0: 
             multiplo = multiplo + 1
-    print("O número",numero ,"tem",multiplo ,"múltiplos.")
-    resposta = str (input("Deseja realizar uma nova consulta, S(s) ou N(n)?"))
+    print("O número",numero ,"tem",multiplo ,"múltiplos. \n")
+    resposta = str (input("Deseja realizar uma nova consulta, S(s) ou N(n)?: "))
+    while (resposta != "S" and resposta != "s" and resposta != "N" and resposta != "n"):
+        resposta = str(input("Deseja realizar uma nova consulta, S(s) ou N(n)?: "))
+    
+    print("\n")
+
 print("Obrigado!!! \n")
