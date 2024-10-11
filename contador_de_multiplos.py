be06fd2d1cc99c7478ee930f55d8cd0e2ec2147e
Gui-Policate <103331414+Gui-Policate@users.noreply.github.com>
2024-10-11 10:38:17 -0300

    Update contador_de_multiplos.py

--- contador_de_multiplos.py
+++ contador_de_multiplos.py
@@ -1,12 +1,23 @@
 print("\n")
 print("Olá, esse é um contador de múltiplos")
 print("Seu objetivo é verificar quanto múltiplos um número tem \n")
-numero = float(input("Por favor informe o número que deseja consultar: "))
-contador = 1
-multiplo = 0
-while contador <= numero:
-    resto = numero % contador 
-    contador = contador + 1
-    if resto == 0: 
-        multiplo = multiplo + 1
-print("O número",numero ,"tem",multiplo ,"múltiplos.")
+
+resposta = "s"
+while resposta == "S" or resposta == "s":
+    numero = float(input("Por favor informe o número que deseja consultar: "))
+    print ("O numero irformado é", numero,".")
+    resposta = str(input("Esta correto, S(s) ou N(n)?"))
+    while resposta == "N" or resposta == "n":
+        numero = float(input("Por favor informe o número que deseja consultar: ""\n"))
+        print ("O numero irformado é", numero,)
+        resposta = str(input("esta correto, S(s) ou N(n)?"))
+    contador = 1
+    multiplo = 0
+    while contador <= numero:
+        resto = numero % contador 
+        contador = contador + 1
+        if resto == 0: 
+            multiplo = multiplo + 1
+    print("O número",numero ,"tem",multiplo ,"múltiplos.")
+    resposta = str (input("Deseja realizar uma nova consulta, S(s) ou N(n)?"))
+print("Obrigado!!! \n")
